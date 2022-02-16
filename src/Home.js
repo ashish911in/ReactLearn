1d925b0a868708b48d1bdcf026476829358a40a4
DESKTOP-3HB0QLR\User 1 <temp@temp.com>
2022-02-16 17:51:34 +1100

    started JSON file

--- src/Home.js
+++ src/Home.js
@@ -7,25 +7,35 @@ const Home = () => {
         {title:'Yes', body:'Lorem ipsmet', author:'any', id: 2}
     ];
     
-    const [blogs, setBlogs] = useState(arrBlogs);
+    const [blogs, setBlogs] = useState(null);
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( blog => blog.id !== id );
         setBlogs(newBlogs);
+        console.log('blogs updated');
     }
 
     const [name, setName] = useState('Mario');
 
     useEffect(() => {
         console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+        console.log(blogs);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            // console.log(res.json());
+            return res.json();
+        }).then(data => {
+            setBlogs(data);
+        }).catch(e => {
+            console.log(`${e.message}`);
+        })
+    }, []);
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            <button onClick={() => setName('Luigi')}>Change Name</button>
-            <p>{ name }</p>
+            { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> }
+            {/* <button onClick={() => setName('Luigi')}>Change Name</button>
+            <p>{ name }</p> */}
         </div>
      );
 }
